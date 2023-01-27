e351a0e43026e99f97ff57ca307321fa75a68b3d
Rishi Raj <rishiraj.1012exp@gmail.com>
2023-01-27 23:27:20 +0530

    added script and new notebooks

--- mathematica/lib/util.m
+++ mathematica/lib/util.m
@@ -1,14 +1,8 @@
 (* ::Package:: *)
 
-(* ::Input::Initialization:: *)
-$K=3;$N=8;
-
-
-(* ::Input::Initialization:: *)
 radiusSimple[qpData_]:=1/2 Total@(#^2&/@qpData[[;;Length[qpData]/2]]);
 
 
-(* ::Input::Initialization:: *)
 radius[tqpData_]:={tqpData//First,1/2 Total@(#^2&/@tqpData[[2;;(Length[tqpData]-1)/2]])};
 
 
@@ -21,7 +15,6 @@ actQP[f_, tqpData_] := Module[{res}, (
 )];
 
 
-(* ::Input::Initialization:: *)
 angMom[tqpData_]:=Module[{q, p}, (
 q = tqpData[[2;;(Length[tqpData]+1)/2]];
 p =tqpData[[1+(Length[tqpData]+1)/2;;]];
@@ -32,22 +25,19 @@ p =ArrayReshape[p, {$K, $N^2-1}];
 )];
 
 
-(* ::Input::Initialization:: *)
 comm[x1_, x2_, $f_]:=Table[Sum[$f[[a, b, c]]*(x1[[b]]x2[[c]]-x1[[c]]x2[[b]]), {b, 1, $N^2-1}, {c, b+1, $N^2-1}], {a, 1, $N^2-1}];
 
 
-(* ::Input::Initialization:: *)
 energy[tqpData_]:=Module[{q, p, cc}, (
 q = tqpData[[2;;(Length[tqpData]+1)/2]];
 p =tqpData[[1+(Length[tqpData]+1)/2;;]];
 
 q =ArrayReshape[q, {$K, $N^2-1}];
 
-{tqpData//First, 1/4 Total@(#^2&/@p) + 1/4 Sum[Total@(#^2&/@comm[q[[i]], q[[j]], $f]), {i, 1, $K}, {j, i+1, $K}]}
+{tqpData//First, 1/4 Total@(#^2&/@p) + 1/4 Sum[Total@(#^2&/@comm[q[[i]], q[[j]], $fSU]), {i, 1, $K}, {j, i+1, $K}]}
 )];
 
 
-(* ::Input::Initialization:: *)
 process[filename_]:=Module[{data, d, e, \[ScriptCapitalD], msg}, Monitor[(
 msg = "Importing time series";
 data = Import[filename, "Table"];
@@ -65,3 +55,33 @@ Prepend[#[\[ScriptCapitalD]]&/@{Mean, StandardDeviation, Skewness}, e]
 
 
 
+convertToMatrix[suNvec_] := suNvec . (basisSU[]);
+
+
+cMat[tData_] := Module[{}, (
+Table[Join[{tData[[k, 1]]}, convertToMatrix/@Partition[tData[[k,2;;]], $N^2-1]], {k, 1, Length[tData]}]
+)];
+
+
+TrX2[tpqData_] := Module[{t, Xs}, (
+t = tpqData[[1]];
+Xs = tpqData[[2;;($K+1)]];
+
+{t, Table[(Tr[Xs[[i]] . Xs[[j]]] - 1/$K Sum[Tr[Xs[[k]] . Xs[[k]]], {k, 1, $K}]KroneckerDelta[i, j])//Chop, {i, 1, $K}, {j, 1, $K}]}
+)];
+
+
+TrX3[tpqData_] := Module[{t, Xs}, (
+t = tpqData[[1]];
+Xs = tpqData[[2;;($K+1)]];
+
+{t, Table[Tr[Xs[[i]] . Xs[[j]] . Xs[[k]]]//Chop, {i, 1, $K}, {j, 1, $K}, {k, 1, $K}]}
+)];
+
+
+TrX4[tpqData_] := Module[{t, Xs}, (
+t = tpqData[[1]];
+Xs = tpqData[[2;;($K+1)]];
+
+{t, Tr[MatrixPower[Sum[Xs[[i]] . Xs[[i]], {i, 1, $K}], 2]]//Chop}
+)];
