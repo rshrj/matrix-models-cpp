d9f96d55833e7bd95f74432133bbb6c32be3fb9c
Rishi Raj <rishiraj.1012exp@gmail.com>
2023-01-28 19:01:44 +0530

    added new stuff

--- scripts/runNK.py
+++ scripts/runNK.py
@@ -3,7 +3,7 @@
 import os
 
 def main():
-    Nmax, Kmax = 10, 10
+    Nmax, Kmax = 3, 4
 
     for N in range(2, Nmax + 1):
         for K in range(2, Kmax + 1):
@@ -15,12 +15,12 @@ def main():
             filedata = filedata.replace('N = 2', f'N = {N}')
             filedata = filedata.replace('K = 2', f'K = {K}')
 
-            with open(f'./src/TrialRun-Corrected-N{N}K{K}.cpp', 'w') as file:
+            with open(f'./cache/TrialRun-Corrected-N{N}K{K}.cpp', 'w') as file:
                 file.write(filedata)
 
             states = os.listdir(f"./runs/States/N{N}K{K}")
 
-            runcmd = f"(g++ -o ./bin/TrialRun-N{N}K{K} ./src/TrialRun-Corrected-N{N}K{K}.cpp -Iinclude -Wall && " + " && ".join([f"./bin/TrialRun-N{N}K{K} " + x for x in states]) + ") &"
+            runcmd = f"(g++ -o ./bin/TrialRun-N{N}K{K} ./cache/TrialRun-Corrected-N{N}K{K}.cpp -Iinclude -Wall && " + " && ".join([f"./bin/TrialRun-N{N}K{K} " + x for x in states]) + ") &"
 
             print(runcmd)
 
