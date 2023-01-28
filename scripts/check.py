aa2f8a08dfd1489e96d9b45fd15643916c22c721
Rishi Raj <rishiraj.1012exp@gmail.com>
2023-01-28 19:04:13 +0530

    added check script

---
+++ scripts/check.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+
+def main():
+    N, K = 3, 5
+
+    print(f'N = {N}, K = {K}:')
+
+    with open('./src/TrialRun.cpp', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('N = 2', f'N = {N}')
+    filedata = filedata.replace('K = 2', f'K = {K}')
+
+    with open(f'./cache/TrialRun-Corrected-N{N}K{K}.cpp', 'w') as file:
+        file.write(filedata)
+
+    file = f"92225358.dat";
+    states = [file];
+
+    runcmd = f"(g++ -o ./bin/TrialRun-N{N}K{K} ./cache/TrialRun-Corrected-N{N}K{K}.cpp -Iinclude -Wall && " + " && ".join([f"./bin/TrialRun-N{N}K{K} " + x for x in states]) + ") &"
+
+    print(runcmd)
+
+    os.system(runcmd)
+                
+    
+
+if __name__ == "__main__":
+    main()
